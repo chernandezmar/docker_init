1e943cee3f5716ac861cf06a42222769759b6ee9
Carlos Hernandez <carlos.hernandezmar@anahuac.mx>
2022-10-11 13:19:09 -0500

    test

---
+++ app.py
@@ -0,0 +1,55 @@
+# app.py
+from flask import Flask, request, jsonify
+import mysql.connector
+
+app = Flask(__name__)
+
+countries = [
+    {"id": 1, "name": "Thailand", "capital": "Bangkok", "area": 513120},
+    {"id": 2, "name": "Australia", "capital": "Canberra", "area": 7617930},
+    {"id": 3, "name": "Egypt", "capital": "Cairo", "area": 1010408},
+]
+
+def _find_next_id():
+    return max(country["id"] for country in countries) + 1
+
+@app.get("/countries")
+def get_countries():
+    return jsonify(countries)
+
+@app.get("/mysql")
+def get_mysql():
+    mydb = mysql.connector.connect(
+        host="mysql-db-1",
+        user="root",
+        password="example",
+        database="prueba"
+        )
+    mycursor = mydb.cursor()
+
+    mycursor.execute("SELECT * FROM ejemplo1")
+
+    myresult = mycursor.fetchall()
+
+    for x in myresult:
+        print(x)
+    
+    return (x)
+
+@app.route('/hello', methods = ['GET'])
+def hello():
+    name = request.args.get('name')
+    if name is None:
+        text = 'Hello!'
+    else:
+        text = 'Hello ' + name + '!'
+    return jsonify({"message": text})
+
+@app.post("/countries")
+def add_country():
+    if request.is_json:
+        country = request.get_json()
+        country["id"] = _find_next_id()
+        countries.append(country)
+        return country, 201
+    return {"error": "Request must be JSON"}, 415
